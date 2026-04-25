f745b507fbe84d272bf5ff8f6ee6ea5d920ad61a
Chinthika Rathnayake <chinthika.jayani@gmail.com>
2026-04-26 04:17:43 +1000

    Introduce deliberate test failure - wrong credentials

--- src/test/java/web/service/LoginServiceUnitTest.java
+++ src/test/java/web/service/LoginServiceUnitTest.java
@@ -7,7 +7,7 @@ public class LoginServiceUnitTest {
 
     @Test
     public void testValidLogin() {
-        assertFalse(LoginService.login("chinthika", "chinthika_pass", "1994-08-19"));
+        assertTrue(LoginService.login("wrong_user", "chinthika_pass", "1994-08-19"));
     }
 
     @Test
